5ad7278deefe0392274d457343c1dde1d2e8f6eb
Samuel Adu <theadusamuel@gmail.com>
2025-01-03 20:48:19 +0000

    add mobile navigation

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const menuOpenBtn = document.getElementById("menu__open-btn");
+const menuCloseBtn = document.getElementById("menu__close-btn");
+const nav = document.getElementById("nav");
+const menu = document.getElementById("menu");
+const navLinks = document.querySelectorAll(".nav__link");
+
 document.addEventListener("DOMContentLoaded", () => {
   const tabLinks = document.querySelectorAll(".tab-link");
   const tabContents = document.querySelectorAll(".tab-content");
@@ -14,5 +20,36 @@ document.addEventListener("DOMContentLoaded", () => {
       link.classList.add("active");
       document.getElementById(targetTab).classList.add("active");
     });
+  });
+});
+
+// Toggle menu visibility
+menuOpenBtn.addEventListener("click", function () {
+  // const expanded =
+  //   menuOpenBtn.getAttribute("aria-expanded") === "true" || false;
+  // menuOpenBtn.setAttribute("aria-expanded", !expanded);
+  menuCloseBtn.setAttribute("aria-expanded", true);
+  menuOpenBtn.setAttribute("aria-expanded", true);
+  nav.classList.toggle("nav--open");
+  document.body.classList.toggle("no-scroll");
+});
+
+// Toggle menu visibility
+menuCloseBtn.addEventListener("click", function () {
+  // const expanded =
+  //   menuCloseBtn.getAttribute("aria-expanded") === "true" || false;
+  // menuCloseBtn.setAttribute("aria-expanded", !expanded);
+  menuOpenBtn.setAttribute("aria-expanded", false);
+  menuCloseBtn.setAttribute("aria-expanded", false);
+  nav.classList.toggle("nav--open");
+  document.body.classList.remove("no-scroll");
+});
+
+// Function to close navigation when a link is clicked
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("nav--open");
+    menuCloseBtn.setAttribute("aria-expanded", false);
+    document.body.classList.remove("no-scroll");
   });
 });
